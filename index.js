1474e447652b3458752d5eaffc249b37fdb52d69
sutrakarhemant <130547412+sutrakarhemant@users.noreply.github.com>
2023-07-07 23:31:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
-// env 
-require("dotenv").config();
-
-require("dotenv").config();
 const express = require('express');
-const app = express();
-const port = process.env.PORT
+const router = express.Router();
 
 
-const session = require('express-session');
-
-
-const db = require('./config/mongoose');
-const passport = require('passport');
-const passportJwt = require('./config/passport-jwt-strategy');
-
-
-//using json and urlencoded middlware to parse the json and submitted form
-app.use(express.json());
-app.use(
-    express.urlencoded({ 
-        extnded: true,
-        })
-    );
-
-
-    app.use(session({ 
-        secret: "something",
-        resave: false,
-        saveUninitialized: true
-      }));
-      
-    
-    //initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-//using routes
-app.use('/', require('./routes'));
+router.use('/doctors', require('./doctors'))
+router.use('/patients', require('./patients'))
+router.use('/reports', require('./reports'))
 
 
 
-
-
-app.listen(process.env.PORT, function(err){
-    if(err){
-        console.log('Error', err);
-        return;
-    }
-    console.log(`running on port: ${port}`);
-    return;
-})
+module.exports= router;
